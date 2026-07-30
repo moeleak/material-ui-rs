0f653756e5b7aa1bb311e59600e94414d2ff1d44
MoeLeak <i@leak.moe>
2026-07-30 17:02:31 +0800

    chore: release 0.5.2

--- package.nix
+++ package.nix
@@ -7,7 +7,7 @@
   writableTmpDirAsHomeHook,
 }:
 let
-  version = "0.5.1";
+  version = "0.5.2";
   src = lib.cleanSource ./.;
   cargoMaterialUiSrc = lib.cleanSource ./tools/cargo-material-ui;
   executableSuffix = stdenv.hostPlatform.extensions.executable or "";
